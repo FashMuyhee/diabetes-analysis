40103fb12180e32b42daf1706d55ea800857004c
FashMuyhee <fashmuyidavid@gmail.com>
2021-02-18 16:18:28 +0100

    inital commit
    For Login Screen

---
+++ src/components/Container.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {StyleSheet, View,ImageBackground} from 'react-native';
+import PropTypes from 'prop-types';
+import bg from '../assets/images/container_bg.png';
+
+const Container = ({children, style}) => {
+  const myStyle = {...styles.container, ...style};
+
+  return (
+    <View style={myStyle}>
+      <ImageBackground style={styles.shapeBg} source={bg}>
+        {children}
+      </ImageBackground>
+    </View>
+  );
+};
+
+Container.prototype = {
+  children: PropTypes.element.isRequired,
+  style: PropTypes.object,
+};
+
+export default Container;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingHorizontal: '7%',
+    justifyContent: 'flex-start',
+    height: '100%',
+    width: '100%',
+  },
+  shapeBg: {
+    height: '80%',
+    width: '100%',
+    backgroundColor: 'white',
+    paddingTop: 30,
+  },
+});
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import NavBar from './NavBar';
+import TabBar from './TabBar';
+import ScrollContainer from './ScrollContainer';
+import Container from './Container';
+
+export {NavBar, TabBar,ScrollContainer,Container};
