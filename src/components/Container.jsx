748c25dd6770f3b946baaebbff98bc3dff4a3ae5
FashMuyhee <fashmuyidavid@gmail.com>
2021-03-31 23:29:08 -0700

    restructued

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, ImageBackground} from 'react-native';
+import {StyleSheet, View, ImageBackground, Image} from 'react-native';
 import PropTypes from 'prop-types';
 import bg from '../assets/images/container_bg.png';
 
@@ -8,9 +8,11 @@ const Container = ({children, style}) => {
 
   return (
     <View style={myStyle}>
-      <ImageBackground style={styles.shapeBg} source={bg}>
+      {/*  <ImageBackground style={styles.shapeBg} source={bg}>
         {children}
-      </ImageBackground>
+      </ImageBackground> */}
+      <Image style={styles.shapeBg} source={bg} />
+      <View style={styles.content}>{children}</View>
     </View>
   );
 };
@@ -24,16 +26,22 @@ export default Container;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
-    paddingHorizontal: '7%',
-    justifyContent: 'flex-start',
+    position: 'relative',
     height: '100%',
-    width: '100%',
   },
   shapeBg: {
-    height: '80%',
+    height: '60%',
     width: '100%',
     backgroundColor: 'white',
     paddingTop: 30,
+  },
+  content: {
+    paddingHorizontal: 0,
+    backgroundColor: 'transparent',
+    justifyContent: 'flex-start',
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+    zIndex: 17,
   },
 });
